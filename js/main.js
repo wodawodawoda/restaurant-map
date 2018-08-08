be78618b83a4c46c539718e6da2bc039102a5a52
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-08-08 04:13:47 +0200

    Fix headers semantic level in restaurants display

--- js/main.js
+++ js/main.js
@@ -152,7 +152,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
